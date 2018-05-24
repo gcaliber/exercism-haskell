2fc61b309bd99452959e8e25503118d05e999fb3
Michael Green <mgreen1980@gmail.com>
2018-05-24 01:46:37 -0400

    fix my stupidity

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -4,5 +4,4 @@ import Data.Char
 import Data.List
 
 isPangram :: String -> Bool
-isPangram xs = sort (nub (filter (not . (`elem` nonAlpha)) (map toLower xs))) == ['a'..'z']
-    where nonAlpha = "`~!@#$%^&*()-_=+{}[]\\|;:\'\"<>,./?1234567890 \t\n\r\f\v"
+isPangram xs = sort (nub (filter (`elem` ['a'..'z']) (map toLower xs))) == ['a'..'z']
