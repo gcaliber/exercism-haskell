3fcebb6aba7ce351d53077858ed14ba1afb09063
Michael Green <mgreen1980@gmail.com>
2018-05-26 14:27:48 -0400

    completed grains

---
+++ grains/src/Grains.hs
@@ -0,0 +1,13 @@
+module Grains (square, total) where
+
+square :: Integer -> Maybe Integer
+square n | n < 1     = Nothing
+         | n > 64    = Nothing
+         | otherwise = Just (square' n)
+
+square' :: Integer -> Integer
+square' 1 = 1
+square' n = 2 * square' (n - 1)
+
+total :: Integer
+total = sum (map square' [1..64])
