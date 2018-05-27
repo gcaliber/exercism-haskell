6eb2d35ae71f7429e77d77dd50dfd97e022c1317
Michael Green <mgreen1980@gmail.com>
2018-05-27 18:36:16 -0400

    no change

--- etl/src/ETL.hs
+++ etl/src/ETL.hs
@@ -8,6 +8,3 @@ transform :: Map a String -> Map Char a
 transform legacyData = Map.fromList(concatMap extract (Map.toList legacyData))
     where extract (k, [])   = []
           extract (k, v:vs) = (toLower v, k) : extract (k, vs)
-
-
--- (1, "AEIOU"), (2, "DGSR")
