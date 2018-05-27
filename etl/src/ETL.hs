3796c2caa823cb03bf339c19aa9ef9d0e851d69f
Michael Green <mgreen1980@gmail.com>
2018-05-27 17:21:15 -0400

    completed etl

--- etl/src/ETL.hs
+++ etl/src/ETL.hs
@@ -1,6 +1,13 @@
 module ETL (transform) where
 
+import Data.Char
 import Data.Map (Map)
+import qualified Data.Map as Map
 
 transform :: Map a String -> Map Char a
-transform legacyData = error "You need to implement this function."
+transform legacyData = Map.fromList(concatMap extract (Map.toList legacyData))
+    where extract (k, [])   = []
+          extract (k, v:vs) = (toLower v, k) : extract (k, vs)
+
+
+-- (1, "AEIOU"), (2, "DGSR")
