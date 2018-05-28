c1b5e55d00bba0496ec1c11ed83a9cb3a52a8a4e
Michael Green <mgreen1980@gmail.com>
2018-05-27 21:19:14 -0400

    completed phone number

---
+++ phone-number/src/Phone.hs
@@ -0,0 +1,16 @@
+module Phone (number) where
+
+number :: String -> Maybe String
+number xs = if isValid xs then Just (clean xs) else Nothing
+          where clean xs = let c = filter (`elem` ['0'..'9']) xs
+                           in if length c == 10 then c else tail c
+
+isValid :: String -> Bool
+isValid xs | any (`notElem` ['0'..'9'] ++ "+ .-()") xs = False
+           | any (`notElem` ['0'..'9']) xs = True && isValid (filter (`elem` ['0'..'9']) xs)
+           | length xs > 11 = False
+           | length xs < 10 = False
+           | length xs == 11 && head xs /= '1' = False
+           | length xs == 11 && head xs == '1' = True && isValid (tail xs)
+           | head xs `elem` ['2'..'9'] && xs !! 3 `elem` ['2'..'9'] = True
+           | otherwise = False
