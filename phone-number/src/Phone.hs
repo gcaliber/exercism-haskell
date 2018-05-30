391f832ec0030e049547f498ffe1d5957b75eda5
Michael Green <mgreen1980@gmail.com>
2018-05-30 17:35:57 -0400

    small phone number cleanup

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -7,10 +7,10 @@ number xs = if isValid xs then Just (clean xs) else Nothing
 
 isValid :: String -> Bool
 isValid xs | any (`notElem` ['0'..'9'] ++ "+ .-()") xs = False
-           | any (`notElem` ['0'..'9']) xs = True && isValid (filter (`elem` ['0'..'9']) xs)
+           | any (`notElem` ['0'..'9']) xs = isValid $ filter (`elem` ['0'..'9']) xs
            | length xs > 11 = False
            | length xs < 10 = False
            | length xs == 11 && head xs /= '1' = False
-           | length xs == 11 && head xs == '1' = True && isValid (tail xs)
+           | length xs == 11 && head xs == '1' = isValid (tail xs)
            | head xs `elem` ['2'..'9'] && xs !! 3 `elem` ['2'..'9'] = True
            | otherwise = False
