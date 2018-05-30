8083f044891eca360c89d66729583e1a8d794b0b
Michael Green <mgreen1980@gmail.com>
2018-05-30 17:30:34 -0400

    cleanup nucleotide count

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -2,9 +2,7 @@ module DNA (nucleotideCounts) where
 
 import qualified Data.Map as M
 
-emptyNucleotideMap = M.fromList [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
-
 nucleotideCounts :: String -> Either String (M.Map Char Int)
-nucleotideCounts [] = Right emptyNucleotideMap
-nucleotideCounts xs | any (`notElem` "ACGT") xs = Left "Invalid nucleotide"
+nucleotideCounts xs | any (`notElem` "ACGT") xs = Left $ "Invalid nucleotide, contains invalid characters: " ++ xs
                     | otherwise = Right (foldl (\m x -> M.insertWith (+) x 1 m) emptyNucleotideMap xs)
+                    where emptyNucleotideMap = M.fromList [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
