5fa753b5933fee1929341a296a9c8869e3f8cde8
Michael Green <mgreen1980@gmail.com>
2018-05-30 17:44:13 -0400

    tiny cleanup sum of multiples

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -3,4 +3,4 @@ module SumOfMultiples (sumOfMultiples) where
 import Data.List
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = (sum . nub) (foldr (\ x -> (++) [x, x + x .. limit - 1]) [] factors)
+sumOfMultiples factors limit = sum . nub $ foldr (\ x -> (++) [x, x + x .. limit - 1]) [] factors
