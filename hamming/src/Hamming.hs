4ee0c8a793180c58b53cb5042ed7f41bfbc34d79
Michael Green <mgreen1980@gmail.com>
2018-05-30 18:08:45 -0400

    cleanup and simplfy hamming

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -2,6 +2,4 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys | length xs /= length ys = Nothing
-               | otherwise = Just (d xs ys)
-               where d [] [] = 0
-                     d (x:xs) (y:ys) = if x /= y then 1 + d xs ys else 0 + d xs ys 
+               | otherwise = Just (sum $ zipWith (\x y -> if x == y then 0 else 1) xs ys)
