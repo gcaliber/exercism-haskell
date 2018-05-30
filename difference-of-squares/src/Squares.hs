1b6cfc77d55b3ee156e69a928fb06c79117acf6a
Michael Green <mgreen1980@gmail.com>
2018-05-30 17:02:26 -0400

    difference of squares cleanup

--- difference-of-squares/src/Squares.hs
+++ difference-of-squares/src/Squares.hs
@@ -4,8 +4,8 @@ difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = x * x
-               where x = sum [1..n]
+squareOfSums n = xs * xs
+               where xs = sum [1..n]
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum (map (\x -> x * x) [1..n])
+sumOfSquares n = sum $ map (\x -> x * x) [1..n]
