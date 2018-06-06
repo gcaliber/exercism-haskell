f3eff4b0eacfd1204f06f8f527ab19525ce41083
Michael Green <mgreen1980@gmail.com>
2018-06-05 20:53:34 -0400

    completed kindergarten garden, not totally happy with it

--- kindergarten-garden/src/Garden.hs
+++ kindergarten-garden/src/Garden.hs
@@ -4,16 +4,35 @@ module Garden
     , lookupPlants
     ) where
 
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+
 data Plant = Clover
            | Grass
            | Radishes
            | Violets
            deriving (Eq, Show)
 
-data Garden = Dummy
+type Garden = M.Map String [Plant]
 
 garden :: [String] -> String -> Garden
-garden students plants = error "You need to implement this function."
+garden students plants = garden' students (convertPlants p1 p2)
+    where p1 = head (lines plants)
+          p2 = last (lines plants)
+
+garden' :: [String] -> [[Plant]] -> Garden
+garden' _ []          = M.empty
+garden' (x:xs) (y:ys) = M.insert x y (garden' xs ys)
+
+convertPlants :: String -> String -> [[Plant]]
+convertPlants [] [] = []
+convertPlants xs ys = (map unabbrev (take 2 xs ++ take 2 ys)) : (convertPlants (drop 2 xs) (drop 2 ys))
+    where unabbrev 'C' = Clover
+          unabbrev 'G' = Grass
+          unabbrev 'R' = Radishes
+          unabbrev 'V' = Violets
 
 lookupPlants :: String -> Garden -> [Plant]
-lookupPlants student garden = error "You need to implement this function."
+lookupPlants student garden = fromMaybe [] (M.lookup student garden)
+
+
